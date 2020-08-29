789e6f50b64be27a8eec21a251eadd3f2593daac
yash <sharmayash44ys@gmail.com>
2020-08-29 15:54:24 +0530

    done for aws

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:4000/graphql", // for development
-  // baseURL: "/graphql", // uncomment line for aws ec2
+  // baseURL: "http://localhost:4000/graphql", // for development
+  baseURL: "/graphql", // uncomment line for aws ec2
 });
 
 export default Api;
